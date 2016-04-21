57319660e49dbe2e8ee39ca837ff56a82c22de6a
mastermind <cyberdude@mail.ru>
2016-04-21 15:32:18 +0300

    +

---
+++ WaveLengthToColor/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+
+namespace WaveLengthToColor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WaveLength_slider_ValueChanged(null, null);
+        }
+
+        private void WaveLength_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (WaveLength_label == null)
+                return;
+
+            WaveLength_label.Content = String.Format("{0} нм", (int)WaveLength_slider.Value);
+
+            UpdateImage(WaveLength_slider.Value);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="l">Wavelength in nm</param>
+        /// <returns></returns>
+        System.Drawing.Color GetColorFromWaveLength(double l) // RGB <0,1> <- lambda l <400,700> [nm]
+        {
+            double t, r, g, b;
+            r = 0.0; g = 0.0; b = 0.0;
+
+            if ((l >= 400.0) && (l < 410.0)) { t = (l - 400.0) / (410.0 - 400.0); r = +(0.33 * t) - (0.20 * t * t); }
+            else if ((l >= 410.0) && (l < 475.0)) { t = (l - 410.0) / (475.0 - 410.0); r = 0.14 - (0.13 * t * t); }
+            else if ((l >= 545.0) && (l < 595.0)) { t = (l - 545.0) / (595.0 - 545.0); r = +(1.98 * t) - (t * t); }
+            else if ((l >= 595.0) && (l < 650.0)) { t = (l - 595.0) / (650.0 - 595.0); r = 0.98 + (0.06 * t) - (0.40 * t * t); }
+            else if ((l >= 650.0) && (l < 700.0)) { t = (l - 650.0) / (700.0 - 650.0); r = 0.65 - (0.84 * t) + (0.20 * t * t); }
+            if ((l >= 415.0) && (l < 475.0)) { t = (l - 415.0) / (475.0 - 415.0); g = +(0.80 * t * t); }
+            else if ((l >= 475.0) && (l < 590.0)) { t = (l - 475.0) / (590.0 - 475.0); g = 0.8 + (0.76 * t) - (0.80 * t * t); }
+            else if ((l >= 585.0) && (l < 639.0)) { t = (l - 585.0) / (639.0 - 585.0); g = 0.84 - (0.84 * t); }
+            if ((l >= 400.0) && (l < 475.0)) { t = (l - 400.0) / (475.0 - 400.0); b = +(2.20 * t) - (1.50 * t * t); }
+            else if ((l >= 475.0) && (l < 560.0)) { t = (l - 475.0) / (560.0 - 475.0); b = 0.7 - (t) + (0.30 * t * t); }
+
+            r *= 255;
+            g *= 255;
+            b *= 255;
+
+            //return System.Drawing.Color.FromArgb((byte)r, (byte)g, (byte)b);
+            return System.Drawing.Color.FromArgb(0, (byte)r, (byte)g, (byte)b);
+        }
+
+
+        void UpdateImage(double wavelength)
+        {
+            var color = GetColorFromWaveLength(wavelength);
+            Bitmap bmp = new Bitmap((int)Color_image.Width, (int)Color_image.Height);
+            //System.Drawing.Color color = System.Drawing.Color.FromArgb((byte)Slider_R.Value, (byte)Slider_G.Value, (byte)Slider_B.Value);
+
+            for (int x = 0; x < bmp.Width; x++)
+                for (int y = 0; y < bmp.Height; y++)
+                    bmp.SetPixel(x, y, color);
+
+
+            Color_image.Source = BitmapToImageSource(bmp);
+        }
+
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
